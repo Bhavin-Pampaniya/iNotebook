6c22469a674b73c2591b51486f508b709cb21191
Bhavin Pamapaniya <bhavinpampaniya07@gmail.com>
2022-10-31 15:24:52 +0530

    created backend

---
+++ backend/script.js
@@ -0,0 +1,18 @@
+require("./db/connection")
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.use(express.json())
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+app.get("/",(req,res)=>{
+    res.send("hello world");
+})
+
+app.listen(port, () => {
+  console.log(`inotebook is listening at http://localhost:${port}`)
+})
